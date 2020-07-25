e39245ec42fbec2f665150b9297ca48bb64eb593
Miro <aguilera8756@gmail.com>
2020-07-24 18:25:12 -0600

    updated

---
+++ build/precache-manifest.e8ebb1d8d15249679b451c2e8a666fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3bee07d886e655d5dc694526292359",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d02c59f7cacc4577fec0",
+    "url": "/static/css/main.94eb1a5b.chunk.css"
+  },
+  {
+    "revision": "2b4d4f8e4a3315287aa0",
+    "url": "/static/js/2.d89d86b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d89d86b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d02c59f7cacc4577fec0",
+    "url": "/static/js/main.be1465e5.chunk.js"
+  },
+  {
+    "revision": "ed0fae93a1705cfe61bf",
+    "url": "/static/js/runtime-main.61c3aabd.js"
+  }
+]);
